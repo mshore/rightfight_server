329338635911876b164ae1a5d3c0551bbf0b8155
Shoreo <shore.matt77@gmail.com>
2019-10-05 13:57:30 -0700

    db set up

---
+++ app/models.py
@@ -0,0 +1,10 @@
+from app import db
+
+
+class LBoardRecord(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), index=True, unique=True)
+    score = db.Column(db.Integer)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.name)
