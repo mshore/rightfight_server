0b403575148461cc32bf6b21c9ba1b4f52a031e7
Shoreo <shore.matt77@gmail.com>
2019-10-06 00:57:10 -0700

    adding index to score for sorting

--- app/models.py
+++ app/models.py
@@ -4,7 +4,7 @@ from app import db
 class LBoardRecord(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), index=True)
-    score = db.Column(db.Integer)
+    score = db.Column(db.Integer, index=True)
 
     def __repr__(self):
         return '<User {}>'.format(self.name)
