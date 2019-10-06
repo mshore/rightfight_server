824e005bb7f45273f04e5a6de6e77a9b9ce33452
SleepyShaman <mshore@users.noreply.github.com>
2019-10-05 17:18:15 -0700

    update the leaderboard

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from app import db
 
 class LBoardRecord(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), index=True, unique=True)
+    name = db.Column(db.String(64), index=True)
     score = db.Column(db.Integer)
 
     def __repr__(self):
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,27 @@
+from flask import request
+from app.models import LBoardRecord
 from app import app
+from app import db
+import sqlite3
+import json
+
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello, World!"
+
+
+@app.route('/detroitsmash')
+def addtoleaderboard():
+    params = request.args
+    uname = params["uname"]
+    end_score = params["score"]
+    if not uname or not end_score: return
+    rec = LBoardRecord(name=uname, score=int(end_score))
+    db.session.add(rec)
+    db.session.commit()
+    return "no one cares"
+
+
+
