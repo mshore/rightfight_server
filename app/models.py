142df41e37e10aa1cc17022f128fce2dc6ef0609
Shoreo <shore.matt77@gmail.com>
2019-12-18 00:29:19 -0800

    back to formula

--- app/models.py
+++ app/models.py
@@ -1,17 +1,17 @@
-from app import db
+from app import db, login_manager
+from flask_login import UserMixin
 
 
-class LBoardRecord(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), index=True)
-    score = db.Column(db.Integer, index=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
 
     def __repr__(self):
-        return '<User {}>'.format(self.name)
-
-    def tojson(self):
-        res = dict()
-        res["name"] = self.name
-        res["score"] = self.score
-        return res
-
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
--- app/routes.py
+++ app/routes.py
@@ -1,32 +1,8 @@
-from flask import request
-from app.models import LBoardRecord
 from app import app
 from app import db
-import json
 
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello, World!"
-
-
-@app.route('/detroitsmash')
-def addtoleaderboard():
-    params = request.args
-    uuid = params["uuid"]
-    uname = params["uname"]
-    end_score = params["score"]
-    if not uname or not end_score: return
-    rec = LBoardRecord(id=uuid, name=uname, score=int(end_score))
-    db.session.add(rec)
-    db.session.commit()
-    return "no one cares"
-
-
-@app.route('/firedance')
-def getleaderboard():
-    leaderboard = LBoardRecord.query.all()
-    print(len(leaderboard))
-    leaderboard_json = json.dumps([obj.tojson() for obj in leaderboard])
-    return leaderboard_json
